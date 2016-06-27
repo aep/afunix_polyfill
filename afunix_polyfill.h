cfdc4c58e85c60c43ab9ec843b1679ea1cc14015
Arvid E. Picciani <aep@exys.org>
2016-06-27 18:00:58 +0200

    fix a race condition
    close followed by socket() in a differen thread will yield the same fd.
    be more careful about which fds to close D:

--- afunix_polyfill.h
+++ afunix_polyfill.h
@@ -71,13 +71,33 @@ static void *polyfill_thread(void*);
 //only call from thread exit! (or before thread started)
 static void afunix_free_internal(struct afunix_polyfil_t *pf)
 {
-    close(pf->user[0]);
-    close(pf->user[1]);
-    close(pf->address_exchange[0]);
-    close(pf->address_exchange[1]);
+    debug_fprintf(stderr, "[%d] closing fds:\n"
+            "\tpf->actual = %d\n"
+            "\tpf->user[0] = %d\n"
+            "\tpf->user[1] = %d\n"
+            "\tpf->address_exchange[0] = %d\n"
+            "\tpf->address_exchange[1] = %d\n"
+            ,pf->user[1]
+            ,pf->actual
+            ,pf->user[1], pf->user[1], pf->address_exchange[0], pf->address_exchange[1]);
+
+    if (pf->user[0] != 0)
+        close(pf->user[0]);
+    if (pf->user[1] != 0)
+        close(pf->user[1]);
+    if (pf->address_exchange[0]  != 0)
+        close(pf->address_exchange[0]);
+    if (pf->address_exchange[1]  != 0)
+        close(pf->address_exchange[1]);
+    if (pf->actual != 0)
+        close(pf->actual);
+
     for (int i = 0; i < AFUNIX_POLYFILL_MAX_CONNECTIONS != 0; i++) {
-        if (pf->client_connections[i] != 0)
+        if (pf->client_connections[i] != 0) {
+            debug_fprintf(stderr, "\tpf->client_connections[%d] = %d\n",
+                    i, pf->client_connections[i]);
             close(pf->client_connections[i]);
+        }
     }
     unmap_polyfill(pf);
     free(pf);
@@ -88,7 +108,15 @@ static int afunix_socket (int options)
     struct afunix_polyfil_t *pf = (struct afunix_polyfil_t *)
         calloc(1, sizeof(struct afunix_polyfil_t));
     int ret = socketpair(AF_UNIX, SOCK_DGRAM, 0, pf->user);
+    if (ret != 0) {
+        afunix_free_internal(pf);
+        return -errno;
+    }
     ret     = socketpair(AF_UNIX, SOCK_DGRAM, 0, pf->address_exchange);
+    if (ret != 0) {
+        afunix_free_internal(pf);
+        return -errno;
+    }
 
     pf->actual = socket(AF_UNIX, SOCK_SEQPACKET, 0);
     if (pf->actual < 0) {
@@ -96,13 +124,16 @@ static int afunix_socket (int options)
         return -errno;
     }
 
-    if (fcntl(pf->actual, F_SETFL, fcntl(pf->actual, F_GETFL, 0) | O_NONBLOCK) == -1) {
-        afunix_free_internal(pf);
-        return -errno;
-    }
+    debug_fprintf(stderr, "[%d] created fds:\n"
+            "\tpf->actual = %d\n"
+            "\tpf->user[0] = %d\n"
+            "\tpf->user[1] = %d\n"
+            "\tpf->address_exchange[0] = %d\n"
+            "\tpf->address_exchange[1] = %d\n",
+            pf->actual, pf->user[0], pf->user[1],
+            pf->address_exchange[0], pf->address_exchange[1]);
 
     map_polyfill(pf);
-    pthread_create (&(pf->thread), 0, polyfill_thread, pf);
     return pf->user[1];
 }
 
@@ -111,9 +142,16 @@ static int afunix_close (int fd)
     struct afunix_polyfil_t * pf = mapped_polyfill(fd);
     if (pf == NULL)
         return EINVAL;
+
+    if (pf->actual_mode == 0) {
+        //didnt start thread yet
+        afunix_free_internal(pf);
+        return 0;
+    }
     pthread_t thread = pf->thread;
     write(pf->address_exchange[1], 0, 0);
     close(pf->address_exchange[1]);
+    pf->address_exchange[1] = 0;
     pthread_join(thread,0);
     return 0;
 }
@@ -124,7 +162,7 @@ static int afunix_bind (int fd, const char *name, int options)
 {
     struct afunix_polyfil_t * pf = mapped_polyfill(fd);
     if (pf == NULL)
-        return EINVAL;
+        return -EINVAL;
 
     struct sockaddr_un sa;
     sa.sun_family = AF_UNIX;
@@ -135,13 +173,18 @@ static int afunix_bind (int fd, const char *name, int options)
 
     int len = strlen(sa.sun_path) + sizeof(sa.sun_family);
     if (bind(pf->actual, (struct sockaddr *)&sa, len) != 0)
-        return errno;
+        return -errno;
 
     if (listen(pf->actual, 0) != 0)
-        return errno;
+        return -errno;
+
+    if (fcntl(pf->actual, F_SETFL, fcntl(pf->actual, F_GETFL, 0) | O_NONBLOCK) == -1) {
+        afunix_free_internal(pf);
+        return -errno;
+    }
 
     pf->actual_mode = 2;
-
+    pthread_create (&(pf->thread), 0, polyfill_thread, pf);
     return 0;
 }
 
@@ -149,7 +192,7 @@ static int afunix_connect (int fd, const char *name, int options)
 {
     struct afunix_polyfil_t * pf = mapped_polyfill(fd);
     if (pf == NULL)
-        return EINVAL;
+        return -EINVAL;
 
     struct sockaddr_un sa;
     sa.sun_family = AF_UNIX;
@@ -157,9 +200,15 @@ static int afunix_connect (int fd, const char *name, int options)
 
     int len = strlen(sa.sun_path) + sizeof(sa.sun_family);
     if (connect(pf->actual, (struct sockaddr *)&sa, len) != 0)
-        return errno;
+        return -errno;
+
+    if (fcntl(pf->actual, F_SETFL, fcntl(pf->actual, F_GETFL, 0) | O_NONBLOCK) == -1) {
+        afunix_free_internal(pf);
+        return -errno;
+    }
 
     pf->actual_mode = 1;
+    pthread_create (&(pf->thread), 0, polyfill_thread, pf);
     return 0;
 }
 
@@ -170,7 +219,7 @@ static int afunix_recvfrom (int fd, void *buffer, size_t length,  int flags, int
         return EINVAL;
     *address = 0;
     //clear pending exchanges.
-    while(recv(pf->address_exchange[1], pf->buf,  sizeof(address), MSG_DONTWAIT)>0){}
+    while(recv(pf->address_exchange[1], pf->buf,  sizeof(address), MSG_DONTWAIT) > 0){}
 
     recv(pf->address_exchange[1], address,  sizeof(address), flags);
     return recv(fd, buffer, length, flags);
@@ -187,16 +236,45 @@ static int afunix_sendto   (int fd, void *buffer, size_t length,  int flags, int
 
 //------------------- polyfill backend ----------
 
+static void polyfill_backend_connection(struct afunix_polyfil_t *pf, int connection, int revents)
+{
+    debug_fprintf(stderr, "[%d] connection: %x\n", pf->user[1], revents);
+    errno = 0;
+    int len = recv(connection, pf->buf, sizeof(pf->buf), 0);
+    if (len == 0 ){ // || (len < 0 && (revents & POLLHUP))) {
+        debug_fprintf(stderr, "[%d] \tclosed\n", pf->user[1]);
+        close(connection);
+        for (int i = 0; i < AFUNIX_POLYFILL_MAX_CONNECTIONS != 0; i++) {
+            if (pf->client_connections[i] == connection) {
+                pf->client_connections[i] = 0;
+                pf->client_connections_count -= 1;
+            }
+        }
+        return;
+    }
+    if (len < 0) {
+        if (errno = EAGAIN) {
+            debug_fprintf(stderr, "[%d] \tEAGAIN\n", pf->user[1]);
+            return;
+        }
+        perror("polyfill_backend_connection::read");
+    }
+    debug_fprintf(stderr, "[%d] \tgot %d bytes\n", len, pf->user[1]);
+
+    send(pf->address_exchange[0], &connection, sizeof(connection), 0);
+    send(pf->user[0], pf->buf, len, 0);
+}
+
 static void polyfill_backend_actual(struct afunix_polyfil_t *pf)
 {
-    debug_fprintf(stderr, "> actual (mode:%d)\n", pf->actual_mode);
+    debug_fprintf(stderr, "[%d] actual (mode:%d)\n", pf->user[1], pf->actual_mode);
     if (pf->actual_mode == 1) {
         int len = recv(pf->actual, pf->buf, sizeof(pf->buf), 0);
-        debug_fprintf(stderr, "  got %d bytes\n", len);
+        debug_fprintf(stderr, "\tgot %d bytes\n", len);
         int connection = 0;
         send(pf->address_exchange[0], &connection, sizeof(connection), 0);
         len = send(pf->user[0], pf->buf, len, 0);
-        debug_fprintf(stderr, "  writen %d into user\n", len);
+        debug_fprintf(stderr, "\twriten %d into user\n", len);
         if (len < 0) {
             perror("polyfill_backend_actual::read");
         }
@@ -204,6 +282,9 @@ static void polyfill_backend_actual(struct afunix_polyfil_t *pf)
         struct sockaddr_un clientname;
         unsigned int size = sizeof (clientname);
         int nuw = accept (pf->actual,(struct sockaddr *) &clientname, &size);
+        debug_fprintf(stderr, "[%d] \taccept %d\n", pf->user[1], nuw);
+
+
         if (fcntl(nuw, F_SETFL, fcntl(nuw, F_GETFL, 0) | O_NONBLOCK) == -1) {
             close(nuw);
             perror("afunix_polyfill: fcntl");
@@ -226,29 +307,29 @@ static void polyfill_backend_actual(struct afunix_polyfil_t *pf)
 
 static void polyfill_backend_address_exchange(struct afunix_polyfil_t *pf)
 {
-    debug_fprintf(stderr, "> address_exchange\n");
+    debug_fprintf(stderr, "[%d] address_exchange\n", pf->user[1]);
     int ret = recv(pf->address_exchange[0], &pf->next_send_address, sizeof(pf->next_send_address), 0);
     if (ret < sizeof(pf->next_send_address)) {
-        debug_fprintf(stderr, "  closed\n");
+        debug_fprintf(stderr, "[%d]\taddress_exchange closed. going to exit\n",  pf->user[1]);
         pf->exit = 1;
     }
 }
 
 static void polyfill_backend_user(struct afunix_polyfil_t *pf)
 {
-    debug_fprintf(stderr, "> user (mode:%d)\n", pf->actual_mode);
+    debug_fprintf(stderr, "[%d] user (mode:%d)\n", pf->user[1], pf->actual_mode);
 
     int len = recv(pf->user[0], pf->buf, sizeof(pf->buf), 0);
-    debug_fprintf(stderr, "  got %d bytes\n", len);
+    debug_fprintf(stderr, "[%d]\tgot %d bytes\n",  pf->user[1], len);
     if (len < 1) {
         pf->exit = 1;
         return;
     }
     if (pf->actual_mode == 1) {
         len = send(pf->actual, pf->buf, len, 0);
-        debug_fprintf(stderr, "  writen %d into actual\n", len);
+        debug_fprintf(stderr, "[%d]\twriten %d into actual\n",  pf->user[1],len);
     } else if (pf->actual_mode == 2) {
-        debug_fprintf(stderr, "  target address is %d\n", pf->next_send_address);
+        debug_fprintf(stderr, "[%d]\ttarget address is %d\n",  pf->user[1], pf->next_send_address);
         for (int i = 0; i < AFUNIX_POLYFILL_MAX_CONNECTIONS != 0; i++) {
             if (pf->client_connections[i] != 0) {
                 if (pf->client_connections[i] ==  pf->next_send_address ||
@@ -260,34 +341,6 @@ static void polyfill_backend_user(struct afunix_polyfil_t *pf)
     } else {
         fprintf(stderr, "BUG! in afunix_polyfill: unknown actual mode %d\n", pf->actual_mode);
     }
-}
-
-static void polyfill_backend_connection(struct afunix_polyfil_t *pf, int connection)
-{
-    debug_fprintf(stderr, "> connection\n");
-    int len = recv(connection, pf->buf, sizeof(pf->buf), 0);
-    if (len < 0) {
-        if (errno = EAGAIN) {
-            debug_fprintf(stderr, "  EAGAIN\n");
-            return;
-        }
-        perror("polyfill_backend_connection::read");
-    }
-    debug_fprintf(stderr, "  got %d bytes\n", len);
-
-    if (len < 1) {
-        debug_fprintf(stderr, "  closed\n");
-        close(connection);
-        for (int i = 0; i < AFUNIX_POLYFILL_MAX_CONNECTIONS != 0; i++) {
-            if (pf->client_connections[i] == connection) {
-                pf->client_connections[i] = 0;
-                pf->client_connections_count -= 1;
-            }
-        }
-        return;
-    }
-    send(pf->address_exchange[0], &connection, sizeof(connection), 0);
-    send(pf->user[0], pf->buf, len, 0);
 }
 
 static void *polyfill_thread(void *_pf)
@@ -304,6 +357,7 @@ static void *polyfill_thread(void *_pf)
 
         int numfds = 3 + pf->client_connections_count;
         struct pollfd fds[numfds];
+        memset(&fds, 0, sizeof(struct pollfd) * numfds);
 
         fds[0].fd = pf->user[0];
         fds[0].events = POLLIN;
@@ -329,9 +383,9 @@ static void *polyfill_thread(void *_pf)
             abort();
         }
 
-        debug_fprintf(stderr, "- poll\n");
+        debug_fprintf(stderr, "[%d]- poll\n", pf->user[1]);
         int ret = poll(fds, numfds, -1);
-        debug_fprintf(stderr, "- end\n");
+        debug_fprintf(stderr, "[%d]- end %d\n", pf->user[1], ret);
         if (ret < 1) {
             perror("poll");
             continue;
@@ -340,25 +394,30 @@ static void *polyfill_thread(void *_pf)
         connections_count = 0;
         for (int i = 0; i < AFUNIX_POLYFILL_MAX_CONNECTIONS != 0; i++) {
             if (client_connections[i] != 0) {
-                if (fds[3 + connections_count].events & POLLIN) {
-                    debug_fprintf(stderr, "- %d (c %d) is active\n",
+                if (fds[3 + connections_count].revents) {
+                    debug_fprintf(stderr, "[%d] - %d (c %d) is active\n",
+                            pf->user[1],
                             fds[3 + connections_count].fd, connections_count);
-                    polyfill_backend_connection(pf, fds[3 + connections_count].fd);
+                    polyfill_backend_connection(pf, fds[3 + connections_count].fd, 
+                            fds[3 + connections_count].revents);
                 }
             }
             ++connections_count;
         }
-        if (fds[1].revents & POLLIN) {
-            polyfill_backend_address_exchange(pf);
-        }
-        if (fds[0].revents & POLLIN) {
+        if (fds[0].revents) {
+            debug_fprintf(stderr, "[%d] - user is active\n", pf->user[1]);
             polyfill_backend_user(pf);
         }
-        if (fds[2].revents & POLLIN) {
+        if (fds[2].revents) {
+            debug_fprintf(stderr, "[%d] - actual is active\n", pf->user[1]);
             polyfill_backend_actual(pf);
         }
+        if (fds[1].revents) {
+            debug_fprintf(stderr, "[%d] - address_exchange is active\n", pf->user[1]);
+            polyfill_backend_address_exchange(pf);
+        }
     }
-    debug_fprintf(stderr, "polythread backend exits\n");
+    debug_fprintf(stderr, "[%d] polythread backend exits\n", pf->user[1]);
     afunix_free_internal(pf);
 }
 
@@ -408,19 +467,21 @@ static struct afunix_polyfil_t *mapped_polyfill(int fd)
     while (n!=0) {
         if (n->user[1] == fd)
             return n;
+        n = n->next;
     }
     return 0;
 }
 
 static void map_polyfill(struct afunix_polyfil_t *pf)
 {
+    debug_fprintf(stderr, "map %p [%d]\n", pf, pf->user[1]);
     if (polyfills == NULL) {
         polyfills = pf;
         return;
     }
     struct afunix_polyfil_t *n = polyfills;
     for(int i = 0; n != 0; i++) {
-        debug_fprintf(stderr, "  %d -> %p\n", i, n);
+        debug_fprintf(stderr, "  %d >> %p [%d]\n", i, n, n->user[1]);
         if (n->next == NULL) {
             n->next = pf;
             return;
